3bb74800d0358e3676dccb21e019bc7a987d2213
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2021-04-12 20:00:51 -0400

    Archive thumbnail sizing

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -1,13 +1,18 @@
 <?php if(have_posts()): ?>
     <?php while(have_posts()): the_post();?>
-
     <div class="card mb-3">
-        <div class="card-body">
-            <h3><a href="<?php the_permalink()?> "><?php the_title(); ?> </a></h3>
+        <div class="row">
+            <div class="col-4 align-content-center">
+                <img class="my-5 mx-3 img-fluid" src="<?php the_post_thumbnail_url() ?>">
+            </div>
+            <div class="col-8 card-body">
+                <h3><a href="<?php the_permalink()?>" class="text-info"><?php the_title(); ?> </a></h3>
+                <p class="mb-3"><?php echo the_date() ?></p>
 
-            <?php the_excerpt() ?>
+                <?php the_excerpt() ?>
 
-            <a href="<?php the_permalink(); ?>" class="btn btn-dark">Read More </a>
+                <a href="<?php the_permalink(); ?>" class="btn btn-secondary">Read More</a>
+            </div>
         </div>
     </div>
 
